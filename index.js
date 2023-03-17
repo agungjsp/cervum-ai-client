47759d794f5d76a0be1ca947e8bb6d935e227abc
Agung Jayasukma Prasetiyo <jsp.agung@gmail.com>
2023-03-17 12:55:38 +0700

    Change port

--- index.js
+++ index.js
@@ -263,8 +263,8 @@ async function main() {
         res.send('OK');
     });
 
-    app.listen(3000, () => {
-        console.log('Server listening on port 3000');
+    app.listen(8080, () => {
+        console.log('Server listening on port 8080');
     });
 }
 
